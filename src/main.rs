5bab424d274be35046de020642bcf9e2bc2b9474
Diogo Lundberg <dclundberg@gmail.com>
2022-01-01 16:00:36 +0100

    Add player to GameState

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,23 @@ use prelude::*;
 
 struct State {
     map: Map,
+    player: Player,
 }
 
 impl State {
     fn new() -> Self {
-        Self { map: Map::new() }
+        Self {
+            map: Map::new(),
+            player: Player::new(Point::new(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)),
+        }
     }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        self.map.render(ctx)
+        self.map.render(ctx);
+        self.player.render(ctx);
     }
 }
 
