e32ce4d5e099569644a98d7e4c96432771e8d216
Diogo Lundberg <dclundberg@gmail.com>
2022-01-03 08:39:48 +0100

    rustfmt

--- src/map.rs
+++ src/map.rs
@@ -3,46 +3,46 @@ const NUM_TILES: usize = (SCREEN_HEIGHT * SCREEN_WIDTH) as usize;
 
 #[derive(Copy, Clone, PartialEq)]
 pub enum TileType {
-  Wall,
-  Floor,
+    Wall,
+    Floor,
 }
 
 pub struct Map {
-  pub tiles: Vec<TileType>,
+    pub tiles: Vec<TileType>,
 }
 
 pub fn map_index(x: i32, y: i32) -> usize {
-  ((y * SCREEN_WIDTH) + x) as usize
+    ((y * SCREEN_WIDTH) + x) as usize
 }
 
 impl Map {
-  pub fn new() -> Self {
-    Self {
-      tiles: vec![TileType::Floor; NUM_TILES],
-    }
-  }
-
-  pub fn render(&self, ctx: &mut BTerm) {
-    for y in 0..SCREEN_HEIGHT {
-      for x in 0..SCREEN_WIDTH {
-        let index = map_index(x, y);
-        match self.tiles[index] {
-          TileType::Floor => {
-            ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
-          }
-          TileType::Wall => {
-            ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
-          }
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
         }
-      }
     }
-  }
 
-  pub fn in_bounds(&self, point: Point) -> bool {
-    point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
-  }
+    pub fn render(&self, ctx: &mut BTerm) {
+        for y in 0..SCREEN_HEIGHT {
+            for x in 0..SCREEN_WIDTH {
+                let index = map_index(x, y);
+                match self.tiles[index] {
+                    TileType::Floor => {
+                        ctx.set(x, y, YELLOW, BLACK, to_cp437('.'));
+                    }
+                    TileType::Wall => {
+                        ctx.set(x, y, GREEN, BLACK, to_cp437('#'));
+                    }
+                }
+            }
+        }
+    }
 
-  pub fn can_enter_tile(&self, point: Point) -> bool {
-    self.in_bounds(point) && self.tiles[map_index(point.x, point.y)] == TileType::Floor
-  }
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_index(point.x, point.y)] == TileType::Floor
+    }
 }
--- src/map_builder.rs
+++ src/map_builder.rs
@@ -2,13 +2,13 @@ use crate::prelude::*;
 const NUM_ROOMS: usize = 20;
 
 pub struct MapBuilder {
-    pub map : Map,
-    pub rooms : Vec<Rect>,
-    pub player_start : Point,
+    pub map: Map,
+    pub rooms: Vec<Rect>,
+    pub player_start: Point,
 }
 
 impl MapBuilder {
-    fn fill(&mut self, tile : TileType) {
+    fn fill(&mut self, tile: TileType) {
         self.map.tiles.iter_mut().for_each(|t| *t = tile);
     }
 }
